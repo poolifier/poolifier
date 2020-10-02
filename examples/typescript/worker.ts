e9e162504b02f053c805c43e7cd5a4a340730970
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-10-02 08:55:25 +0200

    Add typescript examples

---
+++ examples/typescript/worker.ts
@@ -0,0 +1,29 @@
+import { ThreadWorker } from "poolifier";
+
+export interface MyData {
+  ok: number;
+}
+
+export interface MyResponse {
+  message: string;
+  data: MyData;
+}
+
+class MyThreadWorker extends ThreadWorker<MyData, Promise<MyResponse>> {
+  constructor() {
+    super((data) => this.process(data), {
+      maxInactiveTime: 60_000,
+      async: true,
+    });
+  }
+
+  private async process(data: MyData): Promise<MyResponse> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "Hello from Worker :)", data });
+      }, 10_000);
+    });
+  }
+}
+
+export default new MyThreadWorker();
