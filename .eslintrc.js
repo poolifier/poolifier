ed691f5e518e63e2b771e259a3b4d953260dd83d
Shinigami92 <chrissi92@hotmail.de>
2021-02-08 21:14:06 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,18 @@ module.exports = {
     node: true,
     mocha: true
   },
-  extends: ['standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettierx'],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettierx/standardx',
+    'plugin:prettierx/@typescript-eslint'
+  ],
   rules: {}
 }
