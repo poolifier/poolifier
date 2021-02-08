c8c1dec57db3a8b31ad0ea5a1d5a5a0aa9c70ac1
Shinigami92 <chrissi92@hotmail.de>
2021-02-08 19:09:36 +0100

    Use prettierx and eslint native

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {}
+}
