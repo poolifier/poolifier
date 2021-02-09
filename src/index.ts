e0558c615ab86935cea01b0eafa4f3da3413f1d8
Shinigami92 <chrissi92@hotmail.de>
2021-02-09 13:39:03 +0100

    Export every class and type to public API

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import { DynamicClusterPool } from './pools/cluster/dynamic'
 import { FixedClusterPool } from './pools/cluster/fixed'
 import { DynamicThreadPool } from './pools/thread/dynamic'
 import { FixedThreadPool } from './pools/thread/fixed'
+import { ClusterWorker } from './worker/cluster-worker'
 import { ThreadWorker } from './worker/thread-worker'
 
-export type { FixedClusterPoolOptions } from './pools/cluster/fixed'
+export type { DynamicClusterPoolOptions } from './pools/cluster/dynamic'
+export type {
+  FixedClusterPoolOptions,
+  WorkerWithMessageChannel as ClusterWorkerWithMessageChannel
+} from './pools/cluster/fixed'
 export type { DynamicThreadPoolOptions } from './pools/thread/dynamic'
 export type {
   FixedThreadPoolOptions,
-  WorkerWithMessageChannel
+  WorkerWithMessageChannel as ThreadWorkerWithMessageChannel
 } from './pools/thread/fixed'
 export type { WorkerOptions } from './worker/worker-options'
-export { FixedThreadPool, FixedClusterPool, DynamicThreadPool, ThreadWorker }
+export {
+  FixedThreadPool,
+  FixedClusterPool,
+  DynamicClusterPool,
+  DynamicThreadPool,
+  ThreadWorker,
+  ClusterWorker
+}
