e8aefdf8f8aa6de970254f990f4eea8e03033b4e
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-14 08:54:50 +0100

    Build system: add bundling analysis to dev build
    Also add a minified build to the dev build only for now for testing
    purpose.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,27 @@
 import typescript from 'rollup-plugin-typescript2'
+import analyze from 'rollup-plugin-analyzer'
+import { terser } from 'rollup-plugin-terser'
 import del from 'rollup-plugin-delete'
 
 const isDevelopmentBuild = process.env.BUILD === 'development'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'lib',
-    format: 'cjs',
-    sourcemap: !!isDevelopmentBuild,
-    preserveModules: true,
-    preserveModulesRoot: 'src'
-  },
+  output: [
+    {
+      dir: 'lib',
+      format: 'cjs',
+      sourcemap: !!isDevelopmentBuild,
+      preserveModules: true,
+      preserveModulesRoot: 'src'
+    },
+    isDevelopmentBuild && {
+      file: 'lib.min/index.js',
+      format: 'cjs',
+      sourcemap: !!isDevelopmentBuild,
+      plugins: [terser({ numWorkers: 2 })]
+    }
+  ],
   external: ['async_hooks', 'cluster', 'events', 'worker_threads'],
   plugins: [
     typescript({
@@ -20,7 +30,8 @@ export default {
         : 'tsconfig.json'
     }),
     del({
-      targets: 'lib/*'
-    })
+      targets: ['lib/*', 'lib.min/*']
+    }),
+    isDevelopmentBuild && analyze()
   ]
 }
