2e8504654ee66aefdcce9307386a25c097f7a7c0
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-15 13:14:57 +0100

    Revert "Revert to one build output for coverage on sonar cloud."
    This reverts commit f3d990ba9878e4e6f097844cacba9e8e4b4c9121.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,21 +8,14 @@ const isAnalyze = process.env.ANALYZE
 
 export default {
   input: 'src/index.ts',
-  output: [
-    {
-      dir: 'lib',
-      format: 'cjs',
-      sourcemap: !!isDevelopmentBuild,
-      preserveModules: true,
-      preserveModulesRoot: 'src'
-    }
-    // !isDevelopmentBuild && {
-    //   file: 'lib/index.js',
-    //   format: 'cjs',
-    //   sourcemap: !!isDevelopmentBuild,
-    //   plugins: [terser({ numWorkers: 2 })]
-    // }
-  ],
+  output: {
+    ...(isDevelopmentBuild ? { dir: 'lib' } : { file: 'lib/index.js' }),
+    format: 'cjs',
+    sourcemap: !!isDevelopmentBuild,
+    ...(isDevelopmentBuild && { preserveModules: true }),
+    ...(isDevelopmentBuild && { preserveModulesRoot: 'src' }),
+    ...(!isDevelopmentBuild && { plugins: [terser({ numWorkers: 2 })] })
+  },
   external: ['async_hooks', 'cluster', 'events', 'worker_threads'],
   plugins: [
     typescript({
