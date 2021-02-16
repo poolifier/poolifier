143f3ca22ba9859afed14f6a137d676a4f2cc0a0
Alessandro Pio Ardizio <alessandroardizio94@gmail.com>
2021-02-16 11:19:10 +0100

    Update src/worker/worker-options.ts
    Co-authored-by: Shinigami <chrissi92@hotmail.de>

--- src/worker/worker-options.ts
+++ src/worker/worker-options.ts
@@ -16,7 +16,7 @@ export interface WorkerOptions {
    * The last active time of your worker unit will be updated when a task is submitted to a worker or when a worker terminate a task.
    * If killBehavior is set to HARD this value represents also the timeout for the tasks that you submit to the pool,
    * when this timeout expires your tasks is interrupted and the worker is killed if is not part of the minimum size of the pool.
-   * If killBehavior is set to SOFT your tasks have no timeout and your workers will not be terminated until your task is finished.
+   * If `killBehavior` is set to `SOFT` your tasks have no timeout and your workers will not be terminated until your task is finished.
    *
    * @default 60.000 ms
    */
