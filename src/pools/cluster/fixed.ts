880b48b029a7076a5c58206d1acb8bfead1ded2a
aardizio <alessandroardizio94@gmail.com>
2021-02-17 19:45:34 +0100

    Keep comments

--- src/pools/cluster/fixed.ts
+++ src/pools/cluster/fixed.ts
@@ -59,7 +59,6 @@ export class FixedClusterPool<
   }
 
   protected destroyWorker (worker: Worker): void {
-    // Kill received from the worker, means that no new tasks are submitted to that worker for a while ( > maxInactiveTime)
     this.sendToWorker(worker, { kill: 1 })
     worker.kill()
   }
