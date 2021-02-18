c4f2611b7bae8e3cd04df9ba3f22e6ab5943e488
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-18 12:19:09 +0100

    Use the same attribute name for worker script path to file.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/pools/cluster/dynamic.ts
+++ src/pools/cluster/dynamic.ts
@@ -25,16 +25,16 @@ export class DynamicClusterPool<
    *
    * @param min Minimum number of workers which are always active.
    * @param max Maximum number of workers that can be created by this pool.
-   * @param filename Path to an implementation of a `ClusterWorker` file, which can be relative or absolute.
+   * @param filePath Path to an implementation of a `ClusterWorker` file, which can be relative or absolute.
    * @param opts Options for this dynamic cluster pool. Default: `{ maxTasks: 1000 }`
    */
   public constructor (
     min: number,
     public readonly max: number,
-    filename: string,
+    filePath: string,
     opts: ClusterPoolOptions = { maxTasks: 1000 }
   ) {
-    super(min, filename, opts)
+    super(min, filePath, opts)
   }
 
   /**
