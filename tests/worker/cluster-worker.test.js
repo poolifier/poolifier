5a5f9c7f40e5da554aa611b36ffc02ed37cf8427
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-21 19:49:27 +0100

    Renable skipped test on cluster worker.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/worker/cluster-worker.test.js
+++ tests/worker/cluster-worker.test.js
@@ -3,7 +3,7 @@ const { ClusterWorker } = require('../../lib')
 
 describe('Cluster worker test suite', () => {
   // Skipped because ClusterWorker would be in main instead of non-main worker
-  it.skip('Verify worker has default maxInactiveTime', () => {
+  it('Verify worker has default maxInactiveTime', () => {
     const worker = new ClusterWorker(() => {})
     expect(worker.maxInactiveTime).toEqual(60_000)
   })
