8459b47acf11678a6b0c9d831abe376e64ec411f
aardizio <alessandroardizio94@gmail.com>
2021-02-21 12:17:52 +0100

    Exclude no missing require in some bench files

--- benchmarks/versus-external-pools/workers/poolifier/json-stringify.worker.js
+++ benchmarks/versus-external-pools/workers/poolifier/json-stringify.worker.js
@@ -1,4 +1,5 @@
 'use strict'
+/* eslint-disable node/no-missing-require */
 const { ThreadWorker } = require('poolifier')
 const jsonStringify = require('../../functions/json-stringify')
 module.exports = new ThreadWorker(jsonStringify)
