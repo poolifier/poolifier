f9fc9e2d1c523e30bc82605a5c5f77ddaec0f4c2
Shinigami92 <chrissi92@hotmail.de>
2021-02-21 14:31:16 +0100

    Simplify tests

--- tests/pools/selection-strategies.test.js
+++ tests/pools/selection-strategies.test.js
@@ -1,18 +1,6 @@
 const expect = require('expect')
 const { WorkerChoiceStrategies, DynamicThreadPool } = require('../../lib/index')
 const TestUtils = require('../test-utils')
-const min = 1
-const max = 2
-
-const poolWithLessRecentlyUsed = new DynamicThreadPool(
-  min,
-  max,
-  './tests/worker-files/thread/testWorker.js',
-  {
-    maxTasks: 1000,
-    workerChoiceStrategy: WorkerChoiceStrategies.LESS_RECENTLY_USED
-  }
-)
 
 describe('Selection strategies test suite', () => {
   it('Verify that WorkerChoiceStrategies enumeration provides string values', () => {
@@ -21,24 +9,33 @@ describe('Selection strategies test suite', () => {
   })
 
   it('Verify LESS_RECENTLY_USED is taken', async () => {
-    expect(poolWithLessRecentlyUsed.opts.workerChoiceStrategy).toBe(
+    const min = 1
+    const max = 3
+    const pool = new DynamicThreadPool(
+      min,
+      max,
+      './tests/worker-files/thread/testWorker.js',
+      { workerChoiceStrategy: WorkerChoiceStrategies.LESS_RECENTLY_USED }
+    )
+
+    expect(pool.opts.workerChoiceStrategy).toBe(
       WorkerChoiceStrategies.LESS_RECENTLY_USED
     )
 
     // TODO: Create a better test to cover `LessRecentlyUsedWorkerChoiceStrategy#choose`
     const promises = []
-    let fullPool = 0
-    poolWithLessRecentlyUsed.emitter.on('FullPool', () => fullPool++)
     for (let i = 0; i < max * 2; i++) {
-      promises.push(poolWithLessRecentlyUsed.execute({ test: 'test' }))
+      promises.push(pool.execute({ test: 'test' }))
     }
-    expect(poolWithLessRecentlyUsed.workers.length).toBe(max)
-    expect(fullPool > min).toBeTruthy()
-    const res = await TestUtils.waitExits(poolWithLessRecentlyUsed, max - min)
-    expect(res).toBe(max - min)
+    await Promise.all(promises)
+
+    // We need to clean up the resources after our test
+    await pool.destroy()
   })
 
   it('Verify unknown strategies throw error', () => {
+    const min = 1
+    const max = 3
     expect(
       () =>
         new DynamicThreadPool(
