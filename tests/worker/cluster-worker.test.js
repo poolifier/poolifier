5e5c6fcd2e7f2375cff9d2b403099c56d6cb429b
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-21 20:03:37 +0100

    Remove wrong comment.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/worker/cluster-worker.test.js
+++ tests/worker/cluster-worker.test.js
@@ -2,7 +2,6 @@ const expect = require('expect')
 const { ClusterWorker } = require('../../lib')
 
 describe('Cluster worker test suite', () => {
-  // Skipped because ClusterWorker would be in main instead of non-main worker
   it('Verify worker has default maxInactiveTime', () => {
     const worker = new ClusterWorker(() => {})
     expect(worker.maxInactiveTime).toEqual(60_000)
