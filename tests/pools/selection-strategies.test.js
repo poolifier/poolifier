b98ec2e690d5ba33a13a2848b0f1315b885c2f4b
Jérôme Benoit <jerome.benoit@piment-noir.org>
2021-02-22 19:48:46 +0100

    Improve worker selection strategies coverage. (#220)
    * Improve worker selection strategies coverage.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>
    
    * Dedupe packages.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/pools/selection-strategies.test.js
+++ tests/pools/selection-strategies.test.js
@@ -11,18 +11,41 @@ describe('Selection strategies test suite', () => {
     expect(WorkerChoiceStrategies.LESS_RECENTLY_USED).toBe('LESS_RECENTLY_USED')
   })
 
-  it('Verify LESS_RECENTLY_USED strategy is taken', async () => {
+  it('Verify LESS_RECENTLY_USED strategy is taken at pool creation', async () => {
     const max = 3
     const pool = new FixedThreadPool(
       max,
       './tests/worker-files/thread/testWorker.js',
       { workerChoiceStrategy: WorkerChoiceStrategies.LESS_RECENTLY_USED }
     )
-
     expect(pool.opts.workerChoiceStrategy).toBe(
       WorkerChoiceStrategies.LESS_RECENTLY_USED
     )
+    // We need to clean up the resources after our test
+    await pool.destroy()
+  })
 
+  it('Verify LESS_RECENTLY_USED strategy can be set after pool creation', async () => {
+    const max = 3
+    const pool = new FixedThreadPool(
+      max,
+      './tests/worker-files/thread/testWorker.js'
+    )
+    pool.setWorkerChoiceStrategy(WorkerChoiceStrategies.LESS_RECENTLY_USED)
+    expect(pool.opts.workerChoiceStrategy).toBe(
+      WorkerChoiceStrategies.LESS_RECENTLY_USED
+    )
+    // We need to clean up the resources after our test
+    await pool.destroy()
+  })
+
+  it('Verify LESS_RECENTLY_USED strategy can be run in a pool', async () => {
+    const max = 3
+    const pool = new FixedThreadPool(
+      max,
+      './tests/worker-files/thread/testWorker.js',
+      { workerChoiceStrategy: WorkerChoiceStrategies.LESS_RECENTLY_USED }
+    )
     // TODO: Create a better test to cover `LessRecentlyUsedWorkerChoiceStrategy#choose`
     const promises = []
     for (let i = 0; i < max * 2; i++) {
