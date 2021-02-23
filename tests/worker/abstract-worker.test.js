7fc5cce6ed95bfc01cb6199893cf104c3a3c0f0a
Alessandro Pio Ardizio <alessandroardizio94@gmail.com>
2021-02-23 12:45:38 +0100

    Coverage increased (#226)
    * Coverage increased
    
    * Few changes
    
    Co-authored-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/worker/abstract-worker.test.js
+++ tests/worker/abstract-worker.test.js
@@ -1,10 +1,29 @@
 const expect = require('expect')
-const { ClusterWorker } = require('../../lib')
+const { ClusterWorker, ThreadWorker } = require('../../lib')
+
+class StubPoolWithIsMainWorker extends ThreadWorker {
+  constructor (fn, opts) {
+    super(fn, opts)
+    this.mainWorker = false
+  }
+}
 
 describe('Abstract worker test suite', () => {
   it('Verify that fn function is mandatory', () => {
     expect(() => new ClusterWorker()).toThrowError(
       new Error('fn parameter is mandatory')
     )
+  })
+
+  it('Verify that handle Error function is working properly', () => {
+    const error = new Error('My error')
+    const worker = new ThreadWorker(() => {})
+    expect(worker.handleError(error)).toBe(error)
+  })
+
+  it('Verify that get main worker throw error if main worker is not set', () => {
+    expect(() =>
+      new StubPoolWithIsMainWorker(() => {}).getMainWorker()
+    ).toThrowError()
   })
 })
