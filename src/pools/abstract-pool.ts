2248230c9458b08ff38d78a60cd3ef7ff1d3047b
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-28 18:48:58 +0100

    Add sanity checks on pool options.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/pools/abstract-pool.ts
+++ src/pools/abstract-pool.ts
@@ -189,7 +189,7 @@ export abstract class AbstractPool<
         })
         return workerCreated
       },
-      opts.workerChoiceStrategy ?? WorkerChoiceStrategies.ROUND_ROBIN
+      opts?.workerChoiceStrategy ?? WorkerChoiceStrategies.ROUND_ROBIN
     )
   }
 
@@ -217,9 +217,9 @@ export abstract class AbstractPool<
     }
   }
 
-  private checkPoolOptions (opts: PoolOptions<Worker>): void {
+  private checkPoolOptions (opts?: PoolOptions<Worker>): void {
     this.opts.enableEvents =
-      opts.enableEvents !== undefined ? opts.enableEvents : true
+      opts?.enableEvents !== undefined ? opts.enableEvents : true
   }
 
   /** @inheritdoc */
