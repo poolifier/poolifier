03885a7a3d52eeab8e7d9c9028388559c4ce63af
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-28 20:15:53 +0100

    Rename pool options check and affectation.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/pools/abstract-pool.ts
+++ src/pools/abstract-pool.ts
@@ -165,7 +165,7 @@ export abstract class AbstractPool<
     }
     this.checkNumberOfWorkers(this.numberOfWorkers)
     this.checkFilePath(this.filePath)
-    this.checkPoolOptions(opts)
+    this.checkAndAffectPoolOptions(opts)
     this.setupHook()
 
     for (let i = 1; i <= this.numberOfWorkers; i++) {
@@ -219,7 +219,7 @@ export abstract class AbstractPool<
     }
   }
 
-  private checkPoolOptions (opts: PoolOptions<Worker>): void {
+  private checkAndAffectPoolOptions (opts: PoolOptions<Worker>): void {
     this.opts.enableEvents =
       opts.enableEvents != null ? opts.enableEvents : true
   }
