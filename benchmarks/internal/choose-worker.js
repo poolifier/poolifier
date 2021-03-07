c93b4388f3c1da8e381e472e6a3ad68007d502db
Jérôme Benoit <jerome.benoit@sap.com>
2021-03-07 08:48:15 +0100

    Add workers array generator.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- benchmarks/internal/choose-worker.js
+++ benchmarks/internal/choose-worker.js
@@ -3,7 +3,15 @@ const { LIST_FORMATTER } = require('./benchmark-utils')
 
 const suite = new Benchmark.Suite()
 
-const workers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+function generateWorkersArray (numberOfWorkers) {
+  const workers = []
+  for (let i = 0; i < numberOfWorkers; i++) {
+    workers.push(i)
+  }
+  return workers
+}
+
+const workers = generateWorkersArray(60)
 
 let nextWorkerIndex = 0
 
