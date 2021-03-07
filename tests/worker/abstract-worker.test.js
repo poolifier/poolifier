93332c0fef0e47b6ecd9f0c270af46e6bce050b7
Jérôme Benoit <jerome.benoit@sap.com>
2021-03-07 23:21:16 +0100

    Add two tests to assess worker options.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/worker/abstract-worker.test.js
+++ tests/worker/abstract-worker.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect')
-const { ClusterWorker, ThreadWorker } = require('../../lib')
+const { ClusterWorker, KillBehaviors, ThreadWorker } = require('../../lib')
 
 class StubPoolWithIsMainWorker extends ThreadWorker {
   constructor (fn, opts) {
@@ -13,6 +13,24 @@ describe('Abstract worker test suite', () => {
     expect(() => new ClusterWorker()).toThrowError(
       new Error('fn parameter is mandatory')
     )
+  })
+
+  it('Verify worker default values', () => {
+    const worker = new ThreadWorker(() => {})
+    expect(worker.maxInactiveTime).toBe(1000 * 60)
+    expect(worker.killBehavior).toBe(KillBehaviors.SOFT)
+    expect(worker.async).toBe(false)
+  })
+
+  it('Verify that worker options are set at worker creation', () => {
+    const worker = new ClusterWorker(() => {}, {
+      maxInactiveTime: 6000,
+      async: true,
+      killBehavior: KillBehaviors.HARD
+    })
+    expect(worker.maxInactiveTime).toBe(6000)
+    expect(worker.killBehavior).toBe(KillBehaviors.HARD)
+    expect(worker.async).toBe(true)
   })
 
   it('Verify that handleError function is working properly', () => {
