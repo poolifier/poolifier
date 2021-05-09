0baf719f645d96baaca01072b0d36271eb7f52cf
Jérôme Benoit <jerome.benoit@sap.com>
2021-05-09 16:44:34 +0200

    Avoid to sleep during benchmarks
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ benchmarks/versus-external-pools/hyperfine_benchmarks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+hyperfine --export-markdown BENCH-100000.md --min-runs 10 \
+  --prepare 'sleep 15' \
+  'node dynamic-piscina.js' \
+  'node fixed-piscina.js' \
+  'node dynamic-poolifier.js' \
+  'node fixed-poolifier.js' \
+  'node dynamic-suchmokuo-node-worker-threads-pool.js' \
+  'node static-suchmokuo-node-worker-threads-pool.js' \
+  'node threadjs.js' \
+  'node dynamic-workerpool.js' \
+  'node fixed-workerpool.js' \
+  'node fixed-threadwork.js' \
+  'node fixed-microjob.js' \
+  'node fixed-worker-threads-pool.js'
+
+
