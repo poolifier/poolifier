872585ea627ae0f0a0d50e5521233dcb862b66a1
Jérôme Benoit <jerome.benoit@sap.com>
2021-09-17 08:57:17 +0200

    Fix random integer gemerator
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- benchmarks/internal/benchmark-utils.js
+++ benchmarks/internal/benchmark-utils.js
@@ -18,9 +18,9 @@ async function runPoolifierTest (pool, { tasks, workerData }) {
 
 function generateRandomInteger (max, min = 0) {
   if (min) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
-  return Math.floor(Math.random() * max + 1)
+  return Math.floor(Math.random() * (max + 1))
 }
 
 const LIST_FORMATTER = new Intl.ListFormat('en-US', {
