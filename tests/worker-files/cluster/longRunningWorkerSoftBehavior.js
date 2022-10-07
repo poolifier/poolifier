9c8526b801b42a5b40bfa5cec6f6e2082ffc9da7
Jérôme Benoit <jerome.benoit@sap.com>
2022-10-07 22:46:59 +0200

    Remove redundant await
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/worker-files/cluster/longRunningWorkerSoftBehavior.js
+++ tests/worker-files/cluster/longRunningWorkerSoftBehavior.js
@@ -3,7 +3,7 @@ const { ClusterWorker } = require('../../../lib/index')
 const TestUtils = require('../../test-utils')
 
 async function sleep (data) {
-  return await TestUtils.workerSleepFunction(data, 50000)
+  return TestUtils.workerSleepFunction(data, 50000)
 }
 
 module.exports = new ClusterWorker(sleep, {
--- tests/worker-files/thread/longRunningWorkerHardBehavior.js
+++ tests/worker-files/thread/longRunningWorkerHardBehavior.js
@@ -3,7 +3,7 @@ const { ThreadWorker, KillBehaviors } = require('../../../lib/index')
 const TestUtils = require('../../test-utils')
 
 async function sleep (data) {
-  return await TestUtils.workerSleepFunction(data, 50000)
+  return TestUtils.workerSleepFunction(data, 50000)
 }
 
 module.exports = new ThreadWorker(sleep, {
