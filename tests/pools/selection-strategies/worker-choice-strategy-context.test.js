0220f124adb20bbaebb927962b9346daa95a1d96
Jérôme Benoit <jerome.benoit@sap.com>
2022-10-13 21:35:50 +0200

    Add tests for WRR worker choice strategy
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/pools/selection-strategies/worker-choice-strategy-context.test.js
+++ tests/pools/selection-strategies/worker-choice-strategy-context.test.js
@@ -61,11 +61,11 @@ describe('Worker choice strategy context test suite', () => {
       }
     )
     workerChoiceStrategyContext.workerChoiceStrategy = WorkerChoiceStrategyStub
-    const worker = workerChoiceStrategyContext.execute()
+    const chosenWorker = workerChoiceStrategyContext.execute()
     expect(
       workerChoiceStrategyContext.workerChoiceStrategy.choose.calledOnce
     ).toBe(true)
-    expect(worker).toBe('worker')
+    expect(chosenWorker).toBe('worker')
   })
 
   it('Verify that execute() return the worker chosen by the strategy with dynamic pool', () => {
@@ -79,11 +79,11 @@ describe('Worker choice strategy context test suite', () => {
       }
     )
     workerChoiceStrategyContext.workerChoiceStrategy = WorkerChoiceStrategyStub
-    const worker = workerChoiceStrategyContext.execute()
+    const chosenWorker = workerChoiceStrategyContext.execute()
     expect(
       workerChoiceStrategyContext.workerChoiceStrategy.choose.calledOnce
     ).toBe(true)
-    expect(worker).toBe('worker')
+    expect(chosenWorker).toBe('worker')
   })
 
   it('Verify that setWorkerChoiceStrategy() works with ROUND_ROBIN and fixed pool', () => {
