8f98d75d6456164e600ec75240b61ef471217475
Jérôme Benoit <jerome.benoit@sap.com>
2023-03-17 23:31:51 +0100

    build: switch rollup plugins to core ones
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 import analyze from 'rollup-plugin-analyzer'
 import command from 'rollup-plugin-command'
 import del from 'rollup-plugin-delete'
-import { terser } from 'rollup-plugin-terser'
-import ts from 'rollup-plugin-ts'
 
 const isDevelopmentBuild = process.env.BUILD === 'development'
 const isAnalyze = process.env.ANALYZE
@@ -16,15 +16,14 @@ export default {
     sourcemap: !!isDevelopmentBuild,
     ...(isDevelopmentBuild && { preserveModules: true }),
     ...(isDevelopmentBuild && { preserveModulesRoot: 'src' }),
-    ...(!isDevelopmentBuild && { plugins: [terser({ numWorkers: 2 })] })
+    ...(!isDevelopmentBuild && { plugins: [terser({ maxWorkers: 2 })] })
   },
   external: ['async_hooks', 'cluster', 'events', 'os', 'worker_threads'],
   plugins: [
-    ts({
+    typescript({
       tsconfig: isDevelopmentBuild
         ? 'tsconfig.development.json'
-        : 'tsconfig.json',
-      browserslist: false
+        : 'tsconfig.json'
     }),
     del({
       targets: ['lib/*']
