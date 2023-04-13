5634825fd174fb6eebd45159580c4a2a801f637f
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-04-13 17:24:46 +0200

    docs: update benchmarks vs. external pools
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- benchmarks/versus-external-pools/hyperfine_benchmarks.sh
+++ benchmarks/versus-external-pools/hyperfine_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-hyperfine --export-markdown BENCH-100000.md --min-runs 10 \
+/home/linuxbrew/.linuxbrew/bin/hyperfine --export-markdown BENCH-100000.md --min-runs 10 \
   --prepare 'sleep 15' \
   'node dynamic-poolifier.js' \
   'node fixed-poolifier.js' \
