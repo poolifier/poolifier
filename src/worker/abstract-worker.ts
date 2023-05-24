571227f458c744641b0d3f5cfe9d22ed19640e62
Jérôme Benoit <jerome.benoit@sap.com>
2023-05-24 19:34:18 +0200

    test: fix test after async worker options deprecation
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/worker/abstract-worker.ts
+++ src/worker/abstract-worker.ts
@@ -87,6 +87,7 @@ export abstract class AbstractWorker<
     this.opts.killBehavior = opts.killBehavior ?? DEFAULT_KILL_BEHAVIOR
     this.opts.maxInactiveTime =
       opts.maxInactiveTime ?? DEFAULT_MAX_INACTIVE_TIME
+    delete this.opts.async
   }
 
   /**
