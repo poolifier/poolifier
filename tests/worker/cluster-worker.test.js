f4fb3543210a05ecc3dbe1bd9c188d2e09d1756d
Jérôme Benoit <jerome.benoit@sap.com>
2023-06-27 00:37:35 +0200

    test: message sending from cluster worker
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- tests/worker/cluster-worker.test.js
+++ tests/worker/cluster-worker.test.js
@@ -2,6 +2,16 @@ const { expect } = require('expect')
 const { ClusterWorker } = require('../../lib')
 
 describe('Cluster worker test suite', () => {
+  let numberOfMessagesSent = 0
+  const send = function () {
+    ++numberOfMessagesSent
+  }
+  class SpyWorker extends ClusterWorker {
+    getMainWorker () {
+      return { send }
+    }
+  }
+
   it('Verify worker has default maxInactiveTime', () => {
     const worker = new ClusterWorker(() => {})
     expect(worker.opts.maxInactiveTime).toStrictEqual(60000)
@@ -11,5 +21,11 @@ describe('Cluster worker test suite', () => {
     const errorMessage = 'Error as a string'
     const worker = new ClusterWorker(() => {})
     expect(worker.handleError(errorMessage)).toStrictEqual(errorMessage)
+  })
+
+  it('Verify worker invoke the getMainWorker() and send() methods', () => {
+    const worker = new SpyWorker(() => {})
+    worker.sendToMainWorker({ ok: 1 })
+    expect(numberOfMessagesSent).toBe(1)
   })
 })
