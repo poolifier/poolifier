fcb46565163bb679d3abdab62b44d736765935b5
Jérôme Benoit <jerome.benoit@sap.com>
2023-08-11 00:36:26 +0200

    build: silence linter on TS code examples
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -118,6 +118,7 @@ module.exports = defineConfig({
         'import/no-unresolved': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unnecessary-type-assertion': 'off',
