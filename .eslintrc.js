2ab815450d7de12d7d63c46f6192aae2eb6e9b36
Jérôme Benoit <jerome.benoit@sap.com>
2023-08-12 11:16:01 +0200

    docs: make the typedoc generation portable
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,6 +66,7 @@ module.exports = defineConfig({
           'jsdoc',
           'microjob',
           'mjs',
+          'npx',
           'num',
           'os',
           'perf',
---
+++ typedoc.mjs
@@ -0,0 +1,32 @@
+import { copyFileSync, mkdirSync, readdirSync, rmSync } from 'node:fs'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { execSync } from 'node:child_process'
+
+try {
+  mkdirSync(join(dirname(fileURLToPath(import.meta.url)), 'tmp'), {
+    recursive: true
+  })
+  const markdownFiles = readdirSync(
+    join(dirname(fileURLToPath(import.meta.url)), 'docs')
+  ).filter(file => file.endsWith('.md'))
+  for (const markdownFile of markdownFiles) {
+    copyFileSync(
+      join(dirname(fileURLToPath(import.meta.url)), 'docs', markdownFile),
+      join(dirname(fileURLToPath(import.meta.url)), 'tmp', markdownFile)
+    )
+  }
+  execSync('npx typedoc', { stdio: 'inherit' })
+  for (const markdownFile of markdownFiles) {
+    copyFileSync(
+      join(dirname(fileURLToPath(import.meta.url)), 'tmp', markdownFile),
+      join(dirname(fileURLToPath(import.meta.url)), 'docs', markdownFile)
+    )
+  }
+  rmSync(join(dirname(fileURLToPath(import.meta.url)), 'tmp'), {
+    recursive: true,
+    force: true
+  })
+} catch (e) {
+  console.error(e)
+}
