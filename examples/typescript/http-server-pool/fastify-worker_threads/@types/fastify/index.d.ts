1cbc41d7d690ae5a498280b0cb898eeec4522ec6
Jérôme Benoit <jerome.benoit@sap.com>
2023-08-13 14:58:22 +0200

    fix: fix fastify worker_threads type definition
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- examples/typescript/http-server-pool/fastify-worker_threads/@types/fastify/index.d.ts
+++ examples/typescript/http-server-pool/fastify-worker_threads/@types/fastify/index.d.ts
@@ -1,3 +1,4 @@
+import type { TransferListItem } from 'node:worker_threads'
 import type * as fastify from 'fastify'
 import { type DynamicThreadPool } from 'poolifier'
 import { type WorkerData, type WorkerResponse } from '../../src/types.ts'
@@ -5,6 +6,10 @@ import { type WorkerData, type WorkerResponse } from '../../src/types.ts'
 declare module 'fastify' {
   export interface FastifyInstance extends fastify.FastifyInstance {
     pool: DynamicThreadPool<WorkerData, WorkerResponse>
-    execute: (data?: WorkerData, name?: string) => Promise<WorkerResponse>
+    execute: (
+      data?: WorkerData,
+      name?: string,
+      transferList?: TransferListItem[]
+    ) => Promise<WorkerResponse>
   }
 }
