56ea99c2ba591a9797a21e317d666b0f8bce8a66
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-08-25 19:45:01 +0200

    fix: workaround node 16 issue with crypto named export
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import * as os from 'node:os'
-import { getRandomValues } from 'node:crypto'
+import * as crypto from 'node:crypto'
 import type {
   MeasurementStatisticsRequirements,
   WorkerChoiceStrategyOptions
@@ -254,5 +254,5 @@ export const once = (
  * @returns A number in the [0,1[ range
  */
 export const secureRandom = (): number => {
-  return getRandomValues(new Uint32Array(1))[0] / 0x100000000
+  return crypto.getRandomValues(new Uint32Array(1))[0] / 0x100000000
 }
