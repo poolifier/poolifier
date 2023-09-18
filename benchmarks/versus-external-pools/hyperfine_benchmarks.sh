9d698f93d3cb8c2567f9a92aa29ab35d035725b4
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-09-18 13:40:36 +0200

    perf: export also JSON results with external pools benchmark
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- benchmarks/versus-external-pools/hyperfine_benchmarks.sh
+++ benchmarks/versus-external-pools/hyperfine_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-hyperfine --export-markdown BENCH-100000.md --min-runs 20 --prepare 'sleep 5' --warmup 3 --show-output \
+hyperfine --export-markdown BENCH-100000.md --export-json BENCH-100000.json --min-runs 20 --prepare 'sleep 5' --warmup 3 --show-output \
   'node dynamic-poolifier.mjs' \
   'node fixed-poolifier.mjs' \
   'node dynamic-piscina.mjs' \
