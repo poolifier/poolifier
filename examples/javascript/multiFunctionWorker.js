c84b1e6ee62ca0e38ec6a63c0d4ec0b76b9857f8
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-09-28 21:46:46 +0200

    refactor: cleanup javascript example
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- examples/javascript/multiFunctionWorker.js
+++ examples/javascript/multiFunctionWorker.js
@@ -2,13 +2,13 @@
 const { ThreadWorker } = require('poolifier')
 
 function fn0 (data) {
-  console.info('Executing function 0')
-  return { data: `fn0 your input text was '${data.text}'` }
+  console.info('Executing fn0')
+  return { data: `fn0 input text was '${data.text}'` }
 }
 
 function fn1 (data) {
-  console.info('Executing function 1')
-  return { data: `fn1 your input text was '${data.text}'` }
+  console.info('Executing fn1')
+  return { data: `fn1 input text was '${data.text}'` }
 }
 
 module.exports = new ThreadWorker({ fn0, fn1 })
