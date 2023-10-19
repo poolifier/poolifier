535fd8d5bfd0d751e1c40ed77de7514520e89e1f
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-10-19 11:14:41 +0200

    fix: avoid null exception at checking worker node readiness
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - Avoid null exception at sending message to worker.
+- Avoid null exception at checking worker node readiness.
 
 ## [3.0.2] - 2023-10-17
 
--- src/pools/selection-strategies/abstract-worker-choice-strategy.ts
+++ src/pools/selection-strategies/abstract-worker-choice-strategy.ts
@@ -123,7 +123,7 @@ export abstract class AbstractWorkerChoiceStrategy<
    * @returns Whether the worker node is ready or not.
    */
   protected isWorkerNodeReady (workerNodeKey: number): boolean {
-    return this.pool.workerNodes[workerNodeKey]?.info.ready ?? false
+    return this.pool.workerNodes[workerNodeKey]?.info?.ready ?? false
   }
 
   /**
