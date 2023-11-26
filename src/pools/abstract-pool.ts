3a776b6d702aae9831affb9c4fb4084c31bc6657
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-11-26 15:32:11 +0100

    refactor: make worker message listener an arrow function
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/pools/abstract-pool.ts
+++ src/pools/abstract-pool.ts
@@ -1398,7 +1398,7 @@ export abstract class AbstractPool<
     // Listen to worker messages.
     this.registerWorkerMessageListener(
       workerNodeKey,
-      this.workerMessageListener.bind(this)
+      this.workerMessageListener
     )
     // Send the startup message to worker.
     this.sendStartupMessageToWorker(workerNodeKey)
@@ -1667,7 +1667,9 @@ export abstract class AbstractPool<
   /**
    * This method is the message listener registered on each worker.
    */
-  protected workerMessageListener (message: MessageValue<Response>): void {
+  protected readonly workerMessageListener = (
+    message: MessageValue<Response>
+  ): void => {
     this.checkMessageWorkerId(message)
     const { workerId, ready, taskId, taskFunctionNames } = message
     if (ready != null && taskFunctionNames != null) {
