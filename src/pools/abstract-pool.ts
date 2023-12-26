0d03353828857a8d0eb17b318fd817ffb873fc40
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-12-26 19:42:54 +0100

    refactor: cleanup conditions
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/pools/abstract-pool.ts
+++ src/pools/abstract-pool.ts
@@ -1420,10 +1420,7 @@ export abstract class AbstractPool<
   }
 
   private redistributeQueuedTasks (workerNodeKey: number): void {
-    if (workerNodeKey === -1) {
-      return
-    }
-    if (this.cannotStealTask()) {
+    if (workerNodeKey === -1 || this.cannotStealTask()) {
       return
     }
     while (this.tasksQueueSize(workerNodeKey) > 0) {
