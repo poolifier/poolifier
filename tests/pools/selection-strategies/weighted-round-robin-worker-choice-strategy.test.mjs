a1fef95af19c910001ae6cae7218d11282ce9e0a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-01-06 20:18:38 +0100

    test: fix a unit test logic
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/pools/selection-strategies/weighted-round-robin-worker-choice-strategy.test.mjs
+++ tests/pools/selection-strategies/weighted-round-robin-worker-choice-strategy.test.mjs
@@ -21,8 +21,12 @@ describe('Weighted round robin strategy worker choice strategy test suite', () =
 
   it('Verify that reset() resets internals', () => {
     const strategy = new WeightedRoundRobinWorkerChoiceStrategy(pool)
-    strategy.currentWorkerId = randomInt(281474976710655)
-    strategy.workerVirtualTaskRunTime = randomInt(281474976710655)
+    strategy.nextWorkerNodeKey = randomInt(1, 281474976710655)
+    strategy.previousWorkerNodeKey = randomInt(1, 281474976710655)
+    strategy.workerNodeVirtualTaskRunTime = randomInt(1, 281474976710655)
+    expect(strategy.nextWorkerNodeKey).toBeGreaterThan(0)
+    expect(strategy.previousWorkerNodeKey).toBeGreaterThan(0)
+    expect(strategy.workerNodeVirtualTaskRunTime).toBeGreaterThan(0)
     expect(strategy.reset()).toBe(true)
     expect(strategy.nextWorkerNodeKey).toBe(0)
     expect(strategy.previousWorkerNodeKey).toBe(0)
