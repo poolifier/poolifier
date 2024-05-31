f22545899538e05c24ec929bbc16444b7bc23cb0
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-05-31 20:31:06 +0200

    refactor: cleanup type casting
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/worker/abstract-worker.ts
+++ src/worker/abstract-worker.ts
@@ -399,7 +399,7 @@ export abstract class AbstractWorker<
   protected handleKillMessage (message: MessageValue<Data>): void {
     this.stopCheckActive()
     if (isAsyncFunction(this.opts.killHandler)) {
-      ;(this.opts.killHandler() as Promise<void>)
+      ;(this.opts.killHandler as () => Promise<void>)()
         .then(() => {
           this.sendToMainWorker({ kill: 'success' })
           return undefined
