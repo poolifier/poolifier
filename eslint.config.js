afe77cf70299a29a76fe621f998ecf07ec91c6f9
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-05-31 16:49:48 +0200

    build(ci): silencer linter in examples
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- eslint.config.js
+++ eslint.config.js
@@ -79,6 +79,7 @@ export default defineFlatConfig([
   {
     files: ['examples/**/*.ts'],
     rules: {
+      '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
