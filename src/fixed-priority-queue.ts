3cc3a18b6804fd2afc93ef4afe30ca181f06c9a3
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-06-28 01:38:50 +0200

    build(ci): silence linting errors
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/fixed-priority-queue.ts
+++ src/fixed-priority-queue.ts
@@ -129,22 +129,22 @@ export class FixedPriorityQueue<T> {
     return this.nodeArray[index].data
   }
 
-    /**
-     * Deletes the given data from the fixed priority queue.
-     * @param data - Data to delete.
-     * @returns `true` if the data was deleted, `false` otherwise.
-     */
-    public delete (data: T): boolean {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      const index = this.nodeArray.findIndex(node => node?.data === data)
-      if (index !== -1) {
-        this.nodeArray.splice(index, 1)
-        this.nodeArray.length = this.capacity
-        --this.size
-        return true
-      }
-      return false
+  /**
+   * Deletes the given data from the fixed priority queue.
+   * @param data - Data to delete.
+   * @returns `true` if the data was deleted, `false` otherwise.
+   */
+  public delete (data: T): boolean {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    const index = this.nodeArray.findIndex(node => node?.data === data)
+    if (index !== -1) {
+      this.nodeArray.splice(index, 1)
+      this.nodeArray.length = this.capacity
+      --this.size
+      return true
     }
+    return false
+  }
 
   /**
    * Clears the fixed priority queue.
