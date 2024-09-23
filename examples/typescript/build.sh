0634ef644af7ad939b86f26cc8f8c9acc7dc1eae
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-09-23 14:07:49 +0200

    chore: revert incorrect change
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- examples/typescript/build.sh
+++ examples/typescript/build.sh
@@ -7,12 +7,9 @@ examples=$(find . -maxdepth 3 -name "package.json" -exec dirname {} \;)
 for example in $examples
 do
   cd $example
-  # echo -e "Installing dependencies in $example"
-  # pnpm install --ignore-scripts --frozen-lockfile
-  # echo -e "Building $(basename $example)"
-  # pnpm build
-  pnpm update
-  pnpm dedupe
-  pnpm outdated
+  echo -e "Installing dependencies in $example"
+  pnpm install --ignore-scripts --frozen-lockfile
+  echo -e "Building $(basename $example)"
+  pnpm build
   cd -
 done
