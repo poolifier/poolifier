b5a9d39f1d06fa9d41344f6f1519317b4c521e88
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-04-04 13:17:49 +0200

    chore!: drop node 18.x.x support
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- eslint.config.js
+++ eslint.config.js
@@ -56,6 +56,7 @@ export default defineConfig([
       languageOptions: {
         parserOptions: {
           projectService: true,
+          // eslint-disable-next-line n/no-unsupported-features/node-builtins
           tsconfigRootDir: import.meta.dirname,
         },
       },
