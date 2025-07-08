5c5f38057c0a1a7777f0dc31004ce1ca4ab7e999
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-07-08 16:50:24 +0200

    test: refine expectation
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/pools/abstract-pool.test.mjs
+++ tests/pools/abstract-pool.test.mjs
@@ -1480,7 +1480,7 @@ describe('Abstract pool test suite', () => {
     expect(tasksFinished).toBe(0)
     // Worker kill message response timeout is 1000ms
     expect(elapsedTime).toBeLessThanOrEqual(
-      tasksFinishedTimeout + 1000 * tasksFinished + 800
+      tasksFinishedTimeout + 1000 * tasksFinished + 100
     )
   })
 
