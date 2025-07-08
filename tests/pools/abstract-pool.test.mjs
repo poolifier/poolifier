0a30f3645925b2c92057ca6161dafee039664fec
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-07-08 16:48:17 +0200

    test: account properly for kill message timeout
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/pools/abstract-pool.test.mjs
+++ tests/pools/abstract-pool.test.mjs
@@ -1478,7 +1478,10 @@ describe('Abstract pool test suite', () => {
     await pool.destroy()
     const elapsedTime = performance.now() - startTime
     expect(tasksFinished).toBe(0)
-    expect(elapsedTime).toBeLessThanOrEqual(tasksFinishedTimeout + 800)
+    // Worker kill message response timeout is 1000ms
+    expect(elapsedTime).toBeLessThanOrEqual(
+      tasksFinishedTimeout + 1000 * tasksFinished + 800
+    )
   })
 
   it('Verify that pool asynchronous resource track tasks execution', async () => {
